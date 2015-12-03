d0fd13870780d92c3424ad11c14e4eb9f1f9a440
Jon Getahun <Jon@nbp-134-135.nbp.ruw.rutgers.edu>
2015-12-03 14:24:29 -0500

    Added stuff. - JGet

--- myserver.c
+++ myserver.c
@@ -9,31 +9,52 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-/*struct addrinfo {
-    int ai_flags;
-    int ai_family;
-    int ai_socktype;
-    int ai_protocol;
-    size_t ai_addrlen;
-    struct sockaddr * ai_addr;
-    char * ai_canonname;
-    struct addrinfo * ai_next;
-};*/
 int  main () {
-struct addrinfo request, *server;
+    
+    struct addrinfo request, *result, *rp;
 
 /*int getaddrinfo(const char * name, const char * service, const struct addrinfo * request, struct addrinfo ** result);*/
 
-int check;
-check = getaddrinfo("Hello", "server", &request, &server);
+    int check, sd;
 
-request.ai_flags = AI_PASSIVE;
-request.ai_family = AF_INET;
-request.ai_socktype = SOCK_STREAM;
-request.ai_protocol = 0;
-request.ai_addrlen = 0;
-request.ai_addr = NULL;
-request.ai_canonname = NULL;
-request.ai_next = NULL;
+    request.ai_flags = AI_PASSIVE;
+    request.ai_family = AF_INET;
+    request.ai_socktype = SOCK_STREAM;
+    request.ai_protocol = 0;
+    request.ai_addrlen = 0;
+    request.ai_addr = NULL;
+    request.ai_canonname = NULL;
+    request.ai_next = NULL;
+
+    check = getaddrinfo(NULL, "server", &request, &result);
+
+    if (check != 0){
+        fprintf(stderr, "ERROR!\n");
+    }
+
+    for (rp = result; rp != NULL; rp = rp->AI_NEXT) {        
+        sd = socket(request.ai_family, request.ai_socktype, request.ai_protocol);
+
+        if (sd == -1)
+            continue;
+        
+        if (bind(sd, rp->ai_addr, rp->ai_addrlen) == 0)
+            break;
+
+        close(sd);
+    }
+
+    if (rp == NULL) {
+        fprintf(stderr, "ERROR: Could not bind.\n");
+        return 1;
+    }
+
+    freeaddrinfo(result);
+    
+    listen(sd, 100);
+    
+    //accept(sd, (struct sockaddr *)&senderaddr, &ic);
+
+    return 0;
 
 }
