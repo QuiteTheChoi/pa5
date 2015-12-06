6224f6a86b1ec16913fc9fc73da85825367b08a0
Jon Getahun <Jon@nbp-135-95.nbp.ruw.rutgers.edu>
2015-12-06 02:59:21 -0500

    Added stuff. - JGet

--- myserver.c
+++ myserver.c
@@ -30,17 +30,17 @@ bank * myBank;
 
 pthread_mutexattr_t mutattrBank;
 pthread_mutexattr_t mutattrAcct;
-
-//struct bank myBank = myBank;
-//myBank->numAccounts = 0;
+pthread_t bankInfo;
 
 int openAccount(int sock_desc, char name []) {
     
+    char message[500];
+
     while (pthread_mutex_trylock(&myBank->bankLock) != 0) {
         
-        char message[500];
+        //char message[500];
        
-        strcpy(message, "Attempting to open another account. Please wait.\n");
+        strcpy(message, "The bank due to another client search. Please hold.\n");
 
         write(sock_desc, message, sizeof(message)-1);
         
@@ -50,34 +50,89 @@ int openAccount(int sock_desc, char name []) {
     
     if (myBank->numAccounts == 20) {
         pthread_mutex_unlock(&myBank->bankLock);
+        
+        strcpy(message, "The maximum number of accounts has been reached. You may not open one at this time.\n\n");
+
+        write(sock_desc, message, sizeof(message)-1);
+        
         return 1;
     }
     
     else {
 
-        for (int i = 0; i < myBank->numAccounts; i++) {
+        int i;
+
+        for (i = 0; i < myBank->numAccounts; i++) {
             if (strcmp(myBank->accounts[i].name, name) == 0) {
                 pthread_mutex_unlock(&myBank->bankLock);
-                return 2;
+                
+                strcpy(message, "There exists an account with the name that you have provided. You may not open one at this time.\n\n");
+
+                write(sock_desc, message, sizeof(message)-1);
+                
+                return 1;
             }
         }       
         
-        strcpy(myBank->accounts[myBank->numAccounts].name, name);
+        strcpy(myBank->accounts[i].name, name);
+        myBank->accounts[i].balance = 0;
+        myBank->accounts[i].session = 0;
         pthread_mutex_init(&myBank->accounts[myBank->numAccounts].accountLock, &mutattrAcct);
         myBank->numAccounts++;
         pthread_mutex_unlock(&myBank->bankLock);
-        return 0;
-        
+
+        sprintf(message, "Thank you for opening an account with us, %s!\n\n", name);
+        write(sock_desc, message, sizeof(message)-1);
+
+        return 0;       
     }
 }
 
-int startAccount(int sock_desc, char name []) {
+account * startAccount(int sock_desc, char name []) {
 
-    while (pthread_mutex_trylock(&myBank->accounts[myBank->numAccounts].accountLock) != 0) {
+    char message[500];
+
+    while (pthread_mutex_trylock(&myBank->bankLock) != 0) {
         
-        char message[500];
+        //char message[500];
        
-        sprintf(message, "Attempting to start %s's account. Please wait.\n", name);
+        strcpy(message, "The bank is currently locked due to another client search. Please hold.\n");
+
+        write(sock_desc, message, sizeof(message)-1);
+        
+        sleep(2);
+
+    }
+    
+    int i;
+
+    for (i = 0; i < myBank->numAccounts; i++) {
+
+        if (strcmp(myBank->accounts[i].name, name) == 0) {
+            break;
+        }
+
+    }
+
+    pthread_mutex_unlock(&myBank->bankLock);
+
+    if (myBank->numAccounts == i) {
+
+        sprintf(message, "There are no accounts with the name \"%s\" currently open. Please try again later.\n", name);
+
+        write(sock_desc, message, sizeof(message)-1);
+       
+        return NULL;
+
+    }
+
+    else {
+
+        while (pthread_mutex_trylock(&myBank->accounts[i].accountLock) != 0) {
+        
+        //char message[500];
+       
+        sprintf(message, "%s's account is already in session. Please hold.\n", name);
 
         write(sock_desc, message, sizeof(message)-1);
         
@@ -85,85 +140,28 @@ int startAccount(int sock_desc, char name []) {
 
     }
 
-return 0;
+        myBank->accounts[i].session = 1;
+        
+        return &myBank->accounts[i];
+
+    }
 
 }
-
-/*void shmem_create() {
-
-    key_t key;
-    int shmid;
-    //char * p;
-    bank myBank;
-    int	size = 4096;
-    char message[] = "Look at the guy next to you and wake him up.\n";
-    
-    if (errno = 0, (key = ftok( ".", 42)) == -1){
-        printf("ftok() failed  errno :  %s\n", strerror(errno));
-        exit(1);
-    }
-    else if (errno = 0, (shmid = shmget( key, size, 0666 | IPC_CREAT | IPC_EXCL)) != -1){		// create ok?
-        errno = 0;
-        //p = (char *)shmat(shmid, 0, 0);
-        myBank = (struct bank *)shmat(shmid, 0, 0);
-        
-        //if (p == (void *)-1){
-        if (myBank == (void *)-1){
-            printf("shmat() failed  errno :  %s\n", strerror( errno ) );
-            exit(1);
-        }
-        
-        else{
-            // Successful creation of shared memory segment.  Segment is filled with zeros.
-            // Do some interesting initialization.  Something that takes a while.
-            sleep(10);
-            printf("Process %d puts message in created shared memory segment attached at address %#x.\n", getpid(), p + sizeof(int));
-            sprintf(myBank + sizeof(int), "%s", message);
-            *p = 1;
-            //shmdt(p);
-            shmdt(myBank);
-        }
-    }
-    else if (errno = 0, (shmid = shmget( key, 0, 0666 )) != -1){					// find ok?
-        errno = 0;
-        p = (char *)shmat(shmid, 0, 0 );
-        
-        if (p == (void *)-1){
-            printf("shmat() failed  errno :  %s\n", strerror( errno));
-            exit(1);
-        }
-        
-        else{
-            // Acquired shared memory segment.  Has to wait until segment is properly initialized by creator.
-            // Could spin around until initialization complete.
-            while (*p == 0 ){
-                printf("\x1b[2;32mFound segment waiting for initialization to complete.\x1b[0m\n");
-            }
-            printf("Process %d gets message from shared memory segment attached at address %#x.\n", getpid(), p);
-            printf("\n%s\n", p + sizeof(int) );
-            //shmdt(p);
-        }
-    }
-    else{											// no create, no find
-        printf("shmget() failed  errno :  %s\n", strerror(errno));
-        exit(1);
-    }
-    
-    printf("normal end.\n");
-}*/
 
 void client_service(int * sock_desc) {
 
     int sd = *(int *)sock_desc;
 
-    printf("HEY\n");
+    account * tempAccount = NULL;
+
+    //printf("HEY\n");
 
     char buffer[500];
 
     char response[500];
 
     while (read(sd, buffer, sizeof(buffer)-1) > 0) {
-printf("HEY2\n");
+//printf("HEY2\n");
         char command[500];
         char nameOrVal[100];
         
@@ -171,9 +169,21 @@ printf("HEY2\n");
 
         if (strcmp(command, "open") == 0) {
 
-            int result = openAccount(sd, nameOrVal);
+            if (tempAccount != NULL) {
+
+                strcpy(response, "You are already logged in. You may not open an account at this time.\n");
+
+                write(sd, response, sizeof(response)-1);
+
+            }
+
+            else {
             
-            if (result == 0) {
+                openAccount(sd, nameOrVal);
+
+            }
+            
+            /*if (result == 0) {
 
                 sprintf(response, "Thank you for opening an account with us, %s!\n\n", nameOrVal);
 
@@ -194,16 +204,38 @@ printf("HEY2\n");
                 strcpy(response, "There exists an account with the name that you have given. You may not open one at this time.\n\n");
 
                 write(sd, response, sizeof(response)-1);
-            }
+            }*/
 
         }
         
-        /*else if (strcmp(command, "start") == 0) {
+        else if (strcmp(command, "start") == 0) {
 
-          int result = startAccount(clientsd, nameOrVal);
+            if (tempAccount != NULL) {
 
-          }*/
+                strcpy(response, "You are already logged in. You may not open an account at this time.\n");
+
+                write(sd, response, sizeof(response)-1);
+
+            }
+
+            else {
+
+                tempAccount = startAccount(sd, nameOrVal);
+
+            }
+
+        }
     
+    }
+
+}
+
+void printBankInfo() {
+
+    for (int i = 0; i < myBank->numAccounts; i++) {
+
+
+
     }
 
 }
@@ -304,6 +336,11 @@ int main (int argc, char ** argv) {
                 //int clientsd;
 
                 //clientsd = accept(sd, (struct sockaddr *)&saddr, &saddrlen);
+                
+
+                pthread_create(&bankInfo, NULL, (void *) printBankInfo, NULL);
+
+                pthread_join(bankInfo, NULL);
 
                 int pid = fork();
 
@@ -411,6 +448,10 @@ int main (int argc, char ** argv) {
                 //int clientsd;
 
                 //clientsd = accept(sd, (struct sockaddr *)&saddr, &saddrlen);
+
+                pthread_create(&bankInfo, NULL, (void *) printBankInfo, NULL);
+
+                pthread_join(bankInfo, NULL);
 
                 int pid = fork();
 
